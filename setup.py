9953e3fd1203aec4fe154ca57c23ff5eac5c0182
Aritra Ghosh <aritraghsh09@gmail.com>
2020-05-11 17:40:53 -0400

    Initial commit for setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="gamornet-test", # Replace with your own username
+	version="0.0.1",
+	author="Aritra Ghosh",
+	author_email="aritra.ghosh@yale.edu",
+	description="A convolutional neural network to separate bulge and disk-dominated galaxies",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/aritraghsh09/GaMorNet",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+	],
+	python_requires='>=3.6',
+	intall_requires=[
+		"tensorflow-gpu == 1.13.1",
+		"tflearn == 0.3.2",
+		"keras == 2.2.4",
+		"pandas >= 1.0.3",
+		"wget >= 3.2",
+		"numpy >= 1.17.0",
+		"astropy == 3.2.1",
+		"matplotlib >= 3.1.0",
+	],
+
+)
+
