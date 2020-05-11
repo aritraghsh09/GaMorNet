698d4174c1d704bdec7accefb97ab7c1602977d0
Aritra Ghosh <aritraghsh09@gmail.com>
2020-05-11 19:13:02 -0400

    modified some setup options

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="gamornet-test", # Replace with your own username
-	version="0.0.1",
+	version="0.0.5",
 	author="Aritra Ghosh",
 	author_email="aritra.ghosh@yale.edu",
 	description="A convolutional neural network to separate bulge and disk-dominated galaxies",
@@ -17,15 +17,15 @@ setuptools.setup(
 		"Programming Language :: Python :: 3",
 	],
 	python_requires='>=3.6',
-	intall_requires=[
-		"tensorflow-gpu == 1.13.1",
-		"tflearn == 0.3.2",
-		"keras == 2.2.4",
-		"pandas >= 1.0.3",
-		"wget >= 3.2",
-		"numpy >= 1.17.0",
-		"astropy == 3.2.1",
-		"matplotlib >= 3.1.0",
+	install_requires=[
+		"tensorflow-gpu==1.13.1",
+		"tflearn==0.3.2",
+		"keras==2.2.4",
+		"pandas==1.0.3",
+		"wget==3.2",
+		"numpy==1.17.0",
+		"astropy==3.2.1",
+		"matplotlib==3.1.0",
 	],
 
 )
